75015c30b94373f90e1a270abdf1e1c2dc7e9a23
malreddy <malreddy554@gmail.com>
2024-08-01 13:30:50 +0530

    2nd code commit

---
+++ git/script2.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+read -p "enter your name": user_name
+echo
+read -sp "enter your passwd": user_passwd
+echo
+read -p "enter your id ": user_id
+echo
+read -p "enter company name ": company_name
+echo
+read -p "enter your location ": user_location
+echo
+read -p "enter your branch": user_branch
+echo
+read -p "enter your manager": user_manager
+echo
+read -p "enter your project": user_project
+echo
+echo enter name :$user__name
+echo enter passwd:$user_passwd
+echo enter id :$user_id
+echo enter company  :$company_name
+echo enter location :$user_location
+echo enter branch :$user_branch
+echo enter manager :$user_manager 
+echo enter project :$user_project
+
+
+
