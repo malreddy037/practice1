1db182867b9aa337a7f8f85e5580c1290b4f6f65
malreddy <malreddy554@gmail.com>
2024-08-01 13:30:05 +0530

    1st code commit

---
+++ git/script1.sh
@@ -0,0 +1,16 @@
+#! bin/bash
+echo enter your name :
+read user_name
+echo enter your passwd:
+read user_passwd 
+echo enter your id:
+read user_id
+echo enter company:
+read company
+echo enter your location
+read location 
+echo enter name :$user_name
+echo enter passwd:$user_passwd
+echo enter id :$user_id
+echo enter company :$company
+echo enter location:$location
